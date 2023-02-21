96081e05965a1374122e67736a55be349288a0c6
Racheli-BP <4141893@gmail.com>
2023-02-21 17:45:20 +0200

    first

--- testServer/Services/Entities/Person.cs
+++ testServer/Services/Entities/Person.cs
@@ -6,7 +6,7 @@
         public int Id { get; set; }
         public string FullName { get; set; }
         public int YearOfBirth { get; set; }
-        public string Tz { get; set; }
+        public string Tz { get; set; }   
           
     }
 }
